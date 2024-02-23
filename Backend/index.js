290ee994243fd143d7389dce98f51e02e079d1d3
Jorge Mendoza <saasbinoreluxx@gmail.com>
2024-02-23 15:21:20 -0500

    upload a git

---
+++ Backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors'
+
+const app = express();
+app.use(cors())
+
+app.get('/users', (req, res) => {
+    res.send({
+        users: ['Juan Romero']
+    })
+});
+
+app.listen(5000,() =>{
+    console.log("Servidor iniciado en puerto 3000")
+});
---
+++ cliente/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [result, SetResult] = useState("")
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={async() => {
+          const res = await fetch('http://localhost:5000/users');
+          const data = await res.json();
+          console.log(data);
+          SetResult(data)
+        }}>
+          Users
+        </button>
+
+        <p>
+          Haga clic en el botón par ver usuarios.
+        </p>
+
+        <p>
+          {JSON.stringify(result.users,null,2)}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
