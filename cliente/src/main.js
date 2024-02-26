eeada3ffe09ed6d4d1b143c11c0a0919f83a0412
Jorge Mendoza <saasbinoreluxx@gmail.com>
2024-02-26 03:00:05 -0500

    modificado form: fieldset estetica iconos de categoría

--- cliente/src/main.js
+++ cliente/src/main.js
@@ -10,17 +10,20 @@ function MainPage() {
         <div className="container">
             <div className="left-split">
                 <div className="top-left">
+                    <img src="/operador.png" className='category-icon' alt="operador" />
                     <p>Soy operador en campo</p>
                     <Link to='/form' className='Route-link'>
                         <button>Ingresar datos</button>
                     </Link>
                 </div>
                 <div className="bottom-left">
+                    <img src="/consultar.png" className='category-icon' alt="almacenero" />
                     <p>Soy operador de almacén</p>
                     <button>Ingresar datos</button>
                 </div>
             </div>
             <div className="right-split">
+                <img src="/gerente-de-proyecto.png" className='category-icon' alt="adm" />
                 <p>Soy Administrador</p>
                 <button onClick={async () => {
                     const res = await fetch('https://saasbinxx-prototype.onrender.com/users');
