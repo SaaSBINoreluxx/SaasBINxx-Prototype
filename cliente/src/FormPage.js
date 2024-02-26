b1541ed83cb85f2cdb4b2ef521374ec3e725323f
Jorge Mendoza <saasbinoreluxx@gmail.com>
2024-02-26 01:00:44 -0500

    modificado formulario de operador de campo: adición de herramientas y materiales

--- cliente/src/FormPage.js
+++ cliente/src/FormPage.js
@@ -1,19 +1,59 @@
 import React from 'react';
+import './FormPage.css'; 
 
 function FormPage() {
-  return (
-    <div className="form-container">
-      <h2>Ingrese sus datos</h2>
-      <form>
-        <input type="text" placeholder="Nombre" />
-        <input type="text" placeholder="Apellido" />
-        <input type="email" placeholder="Correo electrónico" />
-        <input type="tel" placeholder="Teléfono" />
-        <textarea placeholder="Mensaje"></textarea>
-        <button type="submit">Enviar</button>
-      </form>
-    </div>
-  );
+    const electronicEquipment = ['Multímetro', 'Osciloscopio', 'Destornillador eléctrico', 'Manómetro digital', 'Manómetro analógico', 'Pulmón artificial', 'Monitor 7"', 'Aspiradora de mano'];
+    const tools = ['Destornillador estrella', 'Llave universal 11', 'Llave universal 13', 'Llave universal ajustable'];
+    const materials = ['Manguera corrugada', 'Sensores de oxígeno', 'Precintos', 'Teflón', 'Tornillos', 'Paño absorbente', 'Paño de microfibra', 'Alcohol isopropílico', 'Silicona líquida'];
+
+    return (
+        <div className="form-page">
+            <div className="form-container">
+                <h2>Ingrese sus datos</h2>
+                <form>
+                    <input type="text" placeholder="Nombre" />
+                    <input type="text" placeholder="Apellido" />
+                    <input type="email" placeholder="Correo electrónico" />
+                    <input type="tel" placeholder="Teléfono" />
+                    <textarea placeholder="Mensaje"></textarea>
+                    <button type="submit">Enviar</button>
+                </form>
+            </div>
+            <div className="inventory-container">
+                <h2>Lista de Equipos e Insumos</h2>
+                <form>
+                    <fieldset className='fieldset'>
+                        <legend>Equipos electrónicos</legend>
+                        {electronicEquipment.map((item, index) => (
+                            <div key={index} className='field-element'>
+                                <input type="checkbox" id={`electronic-${index}`} name={item} />
+                                <label htmlFor={`electronic-${index}`}>{item}</label>
+                            </div>
+                        ))}
+                    </fieldset>
+                    <fieldset className='fieldset'>
+                        <legend>Herramientas</legend>
+                        {tools.map((item, index) => (
+                            <div key={index} className='field-element'>
+                                <input type="checkbox" id={`tool-${index}`} name={item} />
+                                <label htmlFor={`tool-${index}`}>{item}</label>
+                            </div>
+                        ))}
+                    </fieldset>
+                    <fieldset className='fieldset'>
+                        <legend>Materiales</legend>
+                        {materials.map((item, index) => (
+                            <div key={index} className='field-element'>
+                                <input type="checkbox" id={`material-${index}`} name={item} />
+                                <label htmlFor={`material-${index}`}>{item}</label>
+                            </div>
+                        ))}
+                    </fieldset>
+                    <button type="submit">Enviar lista</button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
 export default FormPage;
