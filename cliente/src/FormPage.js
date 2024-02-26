bb0109230ef71aa4346b4fa03b97e817242d0403
Jorge Mendoza <saasbinoreluxx@gmail.com>
2024-02-26 15:06:26 -0500

    modificado form: subformulario OTM añadido- estética al 40%

--- cliente/src/FormPage.js
+++ cliente/src/FormPage.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import './FormPage.css'; 
+import React, { useState } from 'react';
+import './FormPage.css';
 
 function FormPage() {
     const electronicEquipment = ['Multímetro', 'Osciloscopio', 'Destornillador eléctrico', 'Manómetro digital', 'Manómetro analógico', 'Pulmón artificial', 'Monitor 7"', 'Aspiradora de mano'];
     const tools = ['Destornillador estrella', 'Llave universal 11', 'Llave universal 13', 'Llave universal ajustable'];
     const materials = ['Manguera corrugada', 'Sensores de oxígeno', 'Precintos', 'Teflón', 'Tornillos', 'Paño absorbente', 'Paño de microfibra', 'Alcohol isopropílico', 'Silicona líquida'];
 
+    const [activities, setActivities] = useState(['']); // Estado para manejar dinámicamente las actividades
+    const [requireSpareParts, setRequireSpareParts] = useState(false); // Estado para manejar si se requieren repuestos
+    const [spareParts, setSpareParts] = useState(['']); // Estado para manejar dinámicamente los repuestos
+
+    const addActivity = () => setActivities([...activities, '']);
+    const removeActivity = (index) => setActivities(activities.filter((_, idx) => idx !== index));
+    const addSparePart = () => setSpareParts([...spareParts, '']);
+    const removeSparePart = (index) => setSpareParts(spareParts.filter((_, idx) => idx !== index));
+
+    // Función para obtener la hora actual en formato HH:MM
+    const getCurrentTime = () => {
+        const now = new Date();
+        return now.toTimeString().split(' ')[0].substring(0, 5);
+    };
+
     return (
         <div className="form-page">
             <div className="form-container">
-                <h2>Ingrese sus datos</h2>
+                <h2>Datos de operador</h2>
                 <form>
                     <input className='field-element' type="text" placeholder="Nombre" />
                     <input className='field-element' type="text" placeholder="Apellido" />
-                    <input className='field-element' type="email" placeholder="Correo electrónico" />
-                    <input className='field-element' type="tel" placeholder="Teléfono" />
-                    <textarea className='field-element'  placeholder="Mensaje"></textarea>
+                    <input className='field-element' type="text" placeholder="ID" />
+                    <textarea className='field-element' placeholder="Mensaje"></textarea>
                     <button type="submit">Enviar</button>
                 </form>
             </div>
@@ -49,8 +63,83 @@ function FormPage() {
                             </div>
                         ))}
                     </fieldset>
-                    <textarea className='field-element'  placeholder="Observaciones"></textarea>
+                    <textarea className='field-element' placeholder="Observaciones"></textarea>
                     <button type="submit">Enviar lista</button>
+                </form>
+            </div>
+            <div className="form-container">
+                <h2>Orden de Trabajo de Mantenimiento</h2>
+                <form>
+                    <fieldset className="fieldset">
+                        <fieldset className="sub-fieldset">
+                            <label for="username">Hora y fecha:</label>
+                            <input type="date" placeholder="Fecha" />
+                            <input type="text" id="horaInicio" placeholder="Hora de inicio de procedimiento" />
+                            <button type="button" onClick={(e) => { e.preventDefault(); e.target.previousElementSibling.value = getCurrentTime(); }}>Obtener hora</button>
+                        </fieldset>
+
+                        <legend>Datos de servicio</legend>
+                        <div>
+                            <label for="username">Entidad:</label>
+                            <input type="text" placeholder="Entidad que recibe el servicio" />
+                        </div>
+                        <input type="text" placeholder="Área" />
+                        <input type="text" placeholder="Ubicación" />
+                    </fieldset>
+
+                    <fieldset className="fieldset">
+                        <legend>Datos del equipo</legend>
+                        <input type="text" placeholder="Denominación del equipo" />
+                        <input type="text" placeholder="Marca" />
+                        <input type="text" placeholder="Modelo" />
+                        <input type="text" placeholder="Serie" />
+                        <input type="text" placeholder="Código patrimonial" />
+                        <select label="Estado inicial" placeholder="Estado inicial">
+                            <option value="Operativo">Operativo</option>
+                            <option value="Inoperativo">Inoperativo</option>
+                            <option value="Parcialmente operativo">Parcialmente operativo</option>
+                        </select>
+                    </fieldset>
+
+                    <fieldset className="fieldset">
+                        <legend>Datos del procedimiento</legend>
+                        {activities.map((activity, index) => (
+                            <div key={index} className="dynamic-field">
+                                <input type="text" placeholder="Descripción de la actividad ejecutada" value={activity} onChange={(e) => {
+                                    const newActivities = [...activities];
+                                    newActivities[index] = e.target.value;
+                                    setActivities(newActivities);
+                                }} />
+                                <button type="button" onClick={() => removeActivity(index)}>Eliminar</button>
+                            </div>
+                        ))}
+                        <button type="button" onClick={addActivity}>Agregar actividad</button>
+
+                        <label for="username">Requirió Repuestos:</label>
+
+                        <select onChange={(e) => setRequireSpareParts(e.target.value === 'sí')}>
+                            <option value="no">No</option>
+                            <option value="sí">Sí</option>
+                        </select>
+
+                        {requireSpareParts && spareParts.map((part, index) => (
+                            <div key={index} className="dynamic-field">
+                                <input type="text" placeholder="Detalle de repuestos" value={part} onChange={(e) => {
+                                    const newSpareParts = [...spareParts];
+                                    newSpareParts[index] = e.target.value;
+                                    setSpareParts(newSpareParts);
+                                }} />
+                                <button type="button" onClick={() => removeSparePart(index)}>Eliminar</button>
+                            </div>
+                        ))}
+                        {requireSpareParts && <button type="button" onClick={addSparePart}>Agregar repuesto</button>}
+
+                        <input type="text" placeholder="Hora de finalización de procedimiento" readOnly />
+                        <button type="button" onClick={(e) => { e.preventDefault(); e.target.previousElementSibling.value = getCurrentTime(); }}>Obtener hora</button>
+                        <textarea placeholder="Observaciones"></textarea>
+                    </fieldset>
+
+                    <button type="submit">Enviar Orden de Trabajo</button>
                 </form>
             </div>
         </div>
