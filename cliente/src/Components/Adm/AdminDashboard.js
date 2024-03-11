8553265cc0ec8249b4c81279a2223182f3fa804c
Jorge Mendoza <saasbinoreluxx@gmail.com>
2024-03-11 15:38:34 -0500

    estructura y estilos de gráficos de dashboard

--- cliente/src/Components/Adm/AdminDashboard.js
+++ cliente/src/Components/Adm/AdminDashboard.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, PieChart, Pie, Cell, Legend, Tooltip } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, PieChart, Pie, Cell, Legend, Tooltip, ResponsiveContainer } from 'recharts';
+import "./AdminDashboard.css"
 
 // Datos de ejemplo para el gráfico
 const data = [
@@ -9,11 +10,10 @@ const data = [
 ];
 
 const dataBarChart = [
-    { name: 'Operador 1', materiales: 400 },
-    { name: 'Operador 2', materiales: 300 },
-    { name: 'Operador 3', materiales: 200 },
-    { name: 'Operador 4', materiales: 278 },
-    { name: 'Operador 5', materiales: 189 },
+    { name: 'Enero', uv: 4000, pv: 2400, amt: 2400 },
+    { name: 'Febrero', uv: 3000, pv: 1398, amt: 2210 },
+    { name: 'Marzo', uv: 2000, pv: 9800, amt: 2290 },
+    // Añade más datos según sea necesario
 ];
 
 const COLORS = ['#ADDF00', '#13FEB3', '#FFBB28', '#FF8042']; // Colores para cada sección del gráfico Doughnut (ahora usando PieChart)
@@ -22,46 +22,54 @@ function AdminDashboard() {
     return (
         <div className='container' >
             <div className="admin-dashboard">
-                <h1>Panel de Administrador</h1>
-                {/* Gráfico Doughnut (usando PieChart) para el progreso de las tareas */}
-                <PieChart width={300} height={300}>
-                    <Pie
-                        data={data}
-                        cx={200}
-                        cy={200}
-                        innerRadius={80}
-                        outerRadius={120}
-                        fill="#8884d8"
-                        paddingAngle={5}
-                        dataKey="value"
-                        label
-                    >
-                        {data.map((entry, index) => (
-                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                        ))}
-                    </Pie>
-                    <Tooltip />
-                    <Legend />
-                </PieChart>
+                <div className='row'>
+                    <div className='chart-container'>
+                        <ResponsiveContainer width="99%" height="100%" aspect={4 / 3}>
+                            <h2>Progreso de tareas</h2>
+                            <PieChart>
+                                <Pie
+                                    data={data}
+                                    cx="50%"
+                                    cy="50%"
+                                    innerRadius="70%"
+                                    outerRadius="80%"
+                                    fill="#8884d8"
+                                    paddingAngle={4}
+                                    dataKey="value"
+                                    label
+                                >
+                                    {data.map((entry, index) => (
+                                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                    ))}
+                                </Pie>
+                                <Tooltip />
+                                <Legend />
+                            </PieChart>
+                        </ResponsiveContainer>
+                    </div>
 
-                <h2>Consumo de Materiales por Operador</h2>
-                <BarChart
-                    width={300}
-                    height={200}
-                    data={dataBarChart}
-                    margin={{
-                        top: 20, right: 30, left: 20, bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="materiales" fill="#00DFC4" />
-                </BarChart>
+                    <div className='chart-container'>
+                        <h2>Consumo de Materiales por Operador</h2>
+                        <ResponsiveContainer width="99%" height="100%" aspect={4 / 3}>
+                            <BarChart data={dataBarChart} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <XAxis dataKey="name" />
+                                <YAxis />
+                                <Tooltip />
+                                <Legend />
+                                <Bar dataKey="pv" stackId="a" fill="#8884d8" barSize={10}/>
+                                <Bar dataKey="uv" stackId="a" fill="#82ca9d" barSize={10}/>
+                                <Bar dataKey="amt" stackId="a" fill="#ffc658" barSize={10}/>
+                            </BarChart>
+                        </ResponsiveContainer>
+                    </div>
+                </div>
 
-                {/* Puedes añadir más gráficos y datos aquí */}
+                <div className='row'>
+                    <div className='chart-container'>
+
+                    </div>
+                </div>
             </div>
         </div>
 
